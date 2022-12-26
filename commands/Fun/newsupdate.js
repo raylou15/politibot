3b47144aa7080d6912e2bffa46befe5243f5973c
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-12-26 17:31:16 -0500

    Update newsupdate.js

--- commands/Fun/newsupdate.js
+++ commands/Fun/newsupdate.js
@@ -19,7 +19,7 @@ const client = (module.exports = {
      */
     async execute(interaction, client) {
       
-        const mainURL = `https://api.newscatcherapi.com/v2/latest_headlines?when=24h?countries=US&topic=politics&lang=en&sources=cnn.com,nytimes.com,foxnews.com,nbcnews.com,politico.com,npr.org,apnews.com,reuters.com,thehill.com,usatoday.com,washingtonpost.com,cbsnews.com`;
+        const mainURL = `https://api.newscatcherapi.com/v2/latest_headlines?when=24h&countries=US&topic=politics&lang=en&sources=cnn.com,nytimes.com,foxnews.com,nbcnews.com,politico.com,npr.org,apnews.com,reuters.com,thehill.com,usatoday.com,washingtonpost.com,cbsnews.com`;
         const mainHeader = {
           "X-API-Key": "Cw83tzJ0P9ibqqTMhXJPXWDcKhZGoW6tKjeWer74oBc",
         }
