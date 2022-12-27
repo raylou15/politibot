dfacafc7978a162efdb50c8f9a688e9ae5e54315
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-12-27 14:58:55 -0500

    Newsletter Framework

--- schemas/messagecount.js
+++
@@ -1,8 +0,0 @@
-const { mongoose, Schema, model } = require("mongoose");
-
-const messagecountSchema = new Schema({
-  UserID: String,
-  msgCount: Number,
-});
-
-module.exports = model("messagecountData", messagecountSchema, "messagecounts");
---
+++ schemas/newslettersubs.js
@@ -0,0 +1,22 @@
+const { mongoose, Schema, model } = require("mongoose");
+
+const newsLetterSchema = new Schema({
+  GuildID: String,
+  UserID: String,
+  CNN: Boolean,
+  Fox: Boolean,
+  NyTimes: Boolean,
+  TheHill: Boolean,
+  NBCNews: Boolean,
+  CBSNews: Boolean,
+  POLITICO: Boolean,
+  NPR: Boolean,
+  APNews: Boolean,
+  Reuters: Boolean,
+  WashPost: Boolean,
+  USAToday: Boolean,
+  OnceADay: Boolean,
+  TwiceADay: Boolean,
+});
+
+module.exports = model("newsletterData", newsLetterSchema, "newslettersubs");
