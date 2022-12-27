dbc591e7e48404caa81f4575ee905ff669d66467
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-12-26 20:15:03 -0500

    Update rank.js

--- commands/Fun/rank.js
+++ commands/Fun/rank.js
@@ -20,32 +20,16 @@ const xp = require('simply-xp')
     async execute(interaction, client) {
         await interaction.deferReply();
 
-        await interaction.deferReply()
+        const member = interaction.options.getUser("user") || interaction.user;
 
-        xp.rank(interaction, interaction.options.getUser("user")?.id || user.id, guild.id, {
-            background: verifyPic(interaction.options.getString("background")) ? interaction.options.getString("background") : null,
-            lvlbarBg: interaction.options.getString("lvlbarbg") || null,
-            lvlbar: interaction.options.getString("lvlbar") || null,
-            color: interaction.options.getString("color") || null,
-        }).then((res) => {
-            interaction.editReply({
-                embeds: [{
-                    title: "Result of Function",
-                    description: "VIEW RANK IN THE ATTACHMENT",
-                    image: { url: "attachment://rank.png" }
-                }], files: [res]
-            })
-        }).catch((err) => {
-            interaction.editReply({
-                embeds: [{
-                    title: "Result of Function",
-                    description: err.toString().substring(0, 1024),
-                    color: "RED"
-                }]
-            })
-        })
-
-        
+        xp.rank(interaction, member.id, interaction.guild.id, {
+            background: 'https://i.imgur.com/nMZf4aT.png',
+            color: "#096DD1",
+            lvlbar: "#FF3FF3",
+            lvlbarBg: "#FF7D33",
+        }).then(async (img) => {
+            return interaction.followUp({ files: [img] })
+        }) 
     },
   });
   
