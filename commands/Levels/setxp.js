12f7bdb0e84704031cd51634e78c0f528f24085b
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-12-26 21:50:17 -0500

    Create setxp.js

---
+++ commands/Levels/setxp.js
@@ -0,0 +1,32 @@
+const {
+    ChatInputCommandInteraction,
+    SlashCommandBuilder,
+    EmbedBuilder,
+    Embed,
+    PermissionFlagsBits,
+  } = require("discord.js");
+const xp = require('simply-xp')
+  const client = (module.exports = {
+    data: new SlashCommandBuilder()
+        .setName("setxp")
+        .setDescription("setxp.")
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addNumberOption(options => options
+            .setName("xp")
+            .setDescription("Which xp?")
+            .setRequired(true) 
+        ),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction, client) {
+        const level = interaction.options.getNumber("xp")
+
+        xp.setXP(message, interaction.user.id, interaction.guild.id, level)
+
+        interaction.reply({ content: "Done!", ephemeral: true })
+
+    },
+});
+  
