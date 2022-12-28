06b0ef3178ab1c74acdb052dfd04ae00205f06c1
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-12-28 00:36:11 -0500

    Newsletter System Complete

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const {
   Partials,
   Collection,
 } = require("discord.js");
+const schedule = require("node-schedule");
 const xp = require("simply-xp");
 const { connect } = require("mongoose");
 const { Guilds, GuildMembers, GuildMessages, MessageContent, DirectMessages, DirectMessageTyping, GuildMessageReactions, GuildMessageTyping,  GuildVoiceStates  } =
@@ -21,6 +22,23 @@ const {
   loadCommands,
   loadComponents,
 } = require("./handlers/handler");
+
+const newsletterhandler = require("./handlers/newsletterhandler")
+
+const rule1 = new schedule.RecurrenceRule();
+rule1.tz = 'America/New_York'
+rule1.second = 5;
+rule1.minute = 0;
+rule1.hour = 8;
+
+const rule2 = new schedule.RecurrenceRule();
+rule2.tz = 'America/New_York'
+rule2.second = 5
+rule2.minute = 0
+rule2.hour = 20
+
+schedule.scheduleJob(rule1, () => {newsletterhandler.MorningNews(client)});
+schedule.scheduleJob(rule2, () => {newsletterhandler.EveningNews(client)});
 
 client.config = require("./config.json");
 client.events = new Collection();
