1487bc6483ce32cc05fdadd505f0bcd3bc31fd14
raylou15 <86377551+raylou15@users.noreply.github.com>
2022-12-27 19:40:41 -0500

    Update newsletter.js

--- commands/Newsletter/newsletter.js
+++ commands/Newsletter/newsletter.js
@@ -32,7 +32,7 @@ const client = (module.exports = {
      */
     async execute(interaction, client) {
 
-        if (!interaction.guild) {
+        if (interaction.guild) {
 
             if (interaction.options.getSubcommand() === 'subscribe') {
 
