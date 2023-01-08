2ff75f8b638271be6c3d36e6b04b1913c90b6815
raylou15 <86377551+raylou15@users.noreply.github.com>
2023-01-07 20:52:24 -0500

    h

---
+++ commands/Utility/maketicketembed.js
@@ -0,0 +1,50 @@
+const {
+    SlashCommandBuilder,
+    ChatInputCommandInteraction,
+    PermissionFlagsBits,
+    EmbedBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ActionRowBuilder,
+  } = require("discord.js");
+  const infractionData = require("../../schemas/infractions");
+  const ms = require("ms");
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName("maketicketembed")
+      .setDescription("Creates the button for verification.")
+      .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+      .addChannelOption((options) =>
+        options
+          .setName("channel")
+          .setDescription("channel to post buttons in")
+          .setRequired(true)
+      ),
+    /**
+     *
+     * @param {ChatInputCommandInteraction} interaction
+     */
+    async execute(interaction, client) {
+      const targetChannel = interaction.options.getChannel("channel");
+      const buttonRow = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+          .setCustomId("openaticket")
+          .setLabel("Open a Ticket")
+          .setStyle(ButtonStyle.Success),
+      );
+  
+      const buttonembed = new EmbedBuilder()
+        .setTitle("Operation Politics Modmail System")
+        .setColor("White")
+        .setDescription("If you need to get in touch with staff, the best way to do it and receive a timely response is using our Modmail / ticket system. This allows you contact our whole team, or specific and relevant parts of our team, to get you the best answers as soon as possible.\n\nOf course, be sure to remember there are other ways of contacting staff for moderation issues, such as `/report` and right clicking a message -> Apps -> Report Message.\n\nYour DMs need to be open and able to accept DMs from bots for this to work. **People who abuse this system will be blacklisted from using it.**")
+        .setFooter({
+          text: "━━━━━━━━━━━━━━━━━ ★ ★ ★ ★ ★ ━━━━━━━━━━━━━━━━━",
+        })
+        .setThumbnail("https://i.imgur.com/kcdKk1U.png");
+  
+      interaction.reply({ content: "thanks", ephemeral: true });
+      targetChannel.send({ embeds: [buttonembed], components: [buttonRow] });
+    },
+  };
+  
---
+++ components/tickets/openaticket.js
@@ -0,0 +1,44 @@
+const { ButtonInteraction, ModalBuilder, ActionRowBuilder, TextInputBuilder, TextInputStyle, EmbedBuilder, ButtonBuilder, ButtonStyle, IntentsBitField } = require("discord.js");
+const config = require("../../config.json")
+const client = module.exports = {
+    name: "openaticket",
+    /**
+     *
+     * @param {ButtonInteraction} interaction
+     */
+    async execute(interaction, client) {
+
+        let truefalseVal = ticketHandler.TicketChecker(client, interaction)
+        console.log(truefalseVal)
+
+        if (truefalseVal === 'true') {
+            return interaction.reply({ content: "You already have a ticket open. Reply in your DMs to the Politibot.", ephemeral: true })
+        }
+
+        const dmErrorEmbed = new EmbedBuilder()
+        .setColor("Red")
+        .setDescription("You have to have your DMs open! All modmail discussions are done through DMs with the Politibot. [See how to open them here](https://www.technobezz.com/how-to-enable-direct-messages-on-discord/) - your settings must be set to 'everyone'");
+        
+        const firstEmbed = new EmbedBuilder()
+        .setColor("Yellow")
+        .setTitle("Operation Politics Modmail System")
+        .setDescription("Please choose the appropriate category below.\n\n`Appeals` are for appealing Moderation Action, `Moderation` is for reporting things or rule inquiries, `Bot Support` is for suggestions or inquiries about the bot, `Partnerships` is to get access to our <#888789135261859851> channel, and `Other` is for everything else.\n\nPlease be wary that abusing this system **will get you blocked**.");
+
+        const buttonsRow = new ActionRowBuilder().addComponents(
+            new ButtonBuilder().setCustomId('appeals').setLabel('Appeals').setStyle(ButtonStyle.Primary).setEmoji('⚖️'),
+            new ButtonBuilder().setCustomId('moderation').setLabel('Moderation').setStyle(ButtonStyle.Danger).setEmoji('🔨'),
+            new ButtonBuilder().setCustomId('botsupport').setLabel('Bot Support').setStyle(ButtonStyle.Success).setEmoji('🤖'),
+            new ButtonBuilder().setCustomId('partnerships').setLabel('Partnerships').setStyle(ButtonStyle.Primary).setEmoji('🤝'),
+            new ButtonBuilder().setCustomId('otherhelp').setLabel('Other').setStyle(ButtonStyle.Secondary).setEmoji('❔')
+        );
+
+        await interaction.user.send({ embeds: [firstEmbed], components: [buttonsRow] }).catch(async (err) => {
+            console.log(err);
+            return interaction.reply({ embeds: [dmErrorEmbed], ephemeral: true });
+        });
+
+        await interaction.reply({ content: "Check your DMs to proceed further!", ephemeral: true})
+
+    },
+  };
+  
