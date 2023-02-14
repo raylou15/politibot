fb8304f06b4f842821956e7ef38e6a7e7351fb18
raylou15 <86377551+raylou15@users.noreply.github.com>
2023-02-14 18:23:33 -0500

    Update levelUp.js

--- events/mainEvents/levelUp.js
+++ events/mainEvents/levelUp.js
@@ -6,9 +6,13 @@ module.exports = {
   name: "levelUp",
   async execute(client, message, data, role) {
 
+    console.log('message')
     console.log(message)
+    console.log('data')
     console.log(data)
+    console.log('role')
     console.log(role)
+    console.log('ok')
 
   }
 }
