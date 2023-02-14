b37cfa69b5cf0c24e1ffdae8f192ba6218163ea9
raylou15 <86377551+raylou15@users.noreply.github.com>
2023-02-14 18:37:14 -0500

    Update levelUp.js

--- events/mainEvents/levelUp.js
+++ events/mainEvents/levelUp.js
@@ -6,9 +6,29 @@ module.exports = {
   name: "levelUp",
   async execute(client, message, data, role) {
 
-    const user = role.guilds.cache.get(message.guildID).members.cache.get(message.userID).user
+    const member = role.guilds.cache.get(message.guildID).members.cache.get(message.userID)
     console.log(user)
 
+    if (!member.user.bot) {
+      const roles = xp.roleSetup.fetch(client, message.guildID)
+      const currentLvl = message.level
+      const chooseroles = [];
+      var bar = new Promise((resolve, reject) => {
+        chooseroles.forEach(role => {
+          member.roles.remove(role.role.toString())
+          if (role.lvl <= currentLvl) {
+            chooseroles.push(role)
+            if (index === array.length -1) resolve();
+          }
+        })
+      })
+      bar.then(() => {
+        member.roles.add(Math.max(...chooseroles.map(role => role.lvl)))
+      })
+
+      console.log("Roles updated for user on level up.")
+
+    }
 
   }
 }
